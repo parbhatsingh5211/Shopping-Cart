3776e5a18a555d46ace5d719f5bdaf58e057baba
parbhatsingh5211 <parbhatsingh.sr@gmail.com>
2024-04-14 00:47:42 +0530

    modify file

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import { Provider } from "react-redux"
 import App from "./App";
 import "./index.css";
 import { store } from "./redux/Store";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Toaster } from 'react-hot-toast'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
       <Provider store={store}>
         <App />
         <Toaster/>
       </Provider>
-  </BrowserRouter>
+  </HashRouter>
   
 );
